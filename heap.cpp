3bba21eb887d02543f55b992c5284e53cd10a821
anisul islam <anisulislam980340@gmail.com>
2023-05-22 07:43:14 +0600

    basic heap memory completed

---
+++ heap.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int * a= new int;
+    float *f= new float;
+    *a=10;
+    *f=2.53;
+    cout<<*a<<endl;
+    cout<<*f<<endl;
+    return 0;
+}
